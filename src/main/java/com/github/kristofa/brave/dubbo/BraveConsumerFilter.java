528ab7c84190266f4d61e941cd4233b7ccf0c3be
liaomengge <liao.mengge@puscene.com>
2017-04-13 17:14:59 +0800

    clean code

--- src/main/java/com/github/kristofa/brave/dubbo/BraveConsumerFilter.java
+++ src/main/java/com/github/kristofa/brave/dubbo/BraveConsumerFilter.java
@@ -3,8 +3,8 @@ package com.github.kristofa.brave.dubbo;
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.rpc.*;
-import com.github.kristofa.brave.*;
-import com.twitter.zipkin.gen.Span;
+import com.github.kristofa.brave.Brave;
+import lombok.Setter;
 
 /**
  * Created by chenjg on 16/7/24.
@@ -12,32 +12,21 @@ import com.twitter.zipkin.gen.Span;
 @Activate(group = Constants.CONSUMER)
 public class BraveConsumerFilter implements Filter {
 
-
-    private static volatile Brave brave;
-    private static volatile String clientName;
-    private static volatile ClientRequestInterceptor clientRequestInterceptor;
-    private static volatile ClientResponseInterceptor clientResponseInterceptor;
-    private static volatile ClientSpanThreadBinder clientSpanThreadBinder;
-
-    public static void setBrave(Brave brave) {
-        BraveConsumerFilter.brave = brave;
-        BraveConsumerFilter.clientRequestInterceptor = brave.clientRequestInterceptor();
-        BraveConsumerFilter.clientResponseInterceptor = brave.clientResponseInterceptor();
-        BraveConsumerFilter.clientSpanThreadBinder = brave.clientSpanThreadBinder();
-    }
+    @Setter
+    private Brave brave;
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        clientRequestInterceptor.handle(new DubboClientRequestAdapter(invoker,invocation));
-        try{
+        brave.clientRequestInterceptor().handle(new DubboClientRequestAdapter(invoker, invocation));
+        try {
             Result rpcResult = invoker.invoke(invocation);
-            clientResponseInterceptor.handle(new DubboClientResponseAdapter(rpcResult));
+            brave.clientResponseInterceptor().handle(new DubboClientResponseAdapter(rpcResult));
             return rpcResult;
-        }catch (Exception ex){
-            clientResponseInterceptor.handle(new DubboClientResponseAdapter(ex));
-            throw  ex;
-        }finally {
-            clientSpanThreadBinder.setCurrentSpan(null);
+        } catch (Exception ex) {
+            brave.clientResponseInterceptor().handle(new DubboClientResponseAdapter(ex));
+            throw ex;
+        } finally {
+            brave.clientSpanThreadBinder().setCurrentSpan(null);
         }
     }
 }
--- src/main/java/com/github/kristofa/brave/dubbo/BraveProviderFilter.java
+++ src/main/java/com/github/kristofa/brave/dubbo/BraveProviderFilter.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.rpc.*;
 import com.github.kristofa.brave.*;
+import lombok.Setter;
 
 /**
  * Created by chenjg on 16/7/24.
@@ -11,27 +12,14 @@ import com.github.kristofa.brave.*;
 @Activate(group = Constants.PROVIDER)
 public class BraveProviderFilter implements Filter {
 
-
-    private static volatile Brave brave;
-    private static volatile ServerRequestInterceptor serverRequestInterceptor;
-    private static volatile ServerResponseInterceptor serverResponseInterceptor;
-    private static volatile ServerSpanThreadBinder serverSpanThreadBinder;
-
-
-
-    public static void setBrave(Brave brave) {
-        BraveProviderFilter.brave = brave;
-        BraveProviderFilter.serverRequestInterceptor = brave.serverRequestInterceptor();
-        BraveProviderFilter.serverResponseInterceptor = brave.serverResponseInterceptor();
-        BraveProviderFilter.serverSpanThreadBinder = brave.serverSpanThreadBinder();
-    }
-
+    @Setter
+    private Brave brave;
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        serverRequestInterceptor.handle(new DubboServerRequestAdapter(invoker,invocation,brave.serverTracer()));
+        brave.serverRequestInterceptor().handle(new DubboServerRequestAdapter(invoker, invocation, brave.serverTracer()));
         Result rpcResult = invoker.invoke(invocation);
-        serverResponseInterceptor.handle(new DubboServerResponseAdapter(rpcResult));
+        brave.serverResponseInterceptor().handle(new DubboServerResponseAdapter(rpcResult));
         return rpcResult;
     }
 }
